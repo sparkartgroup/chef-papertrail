33c07f2ccb41e1a4d995b61eb22b1bb3e56c3c24
Mike Julian <mike@mikejulian.com>
2017-08-10 14:16:57 -0700

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'MIT'
 description      'Installs/Configures Papertrail\'s remote_syslog2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/papertrail/chef-papertrail'
-version          '1.1.0'
+version          '1.1.1'
 issues_url       'https://github.com/papertrail/chef-papertrail/issues'
 chef_version     '>= 12.21'
 supports 'ubuntu', '>= 14.04'
