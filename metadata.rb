113b67e11d178ba06e311caf6dd3070cf6d4f6c5
Tim L <timo614@gmail.com>
2019-04-08 18:17:51 -0400

    Chef 2.18 testing

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/papertrail/chef-papertrail'
 version          '1.1.1'
 issues_url       'https://github.com/papertrail/chef-papertrail/issues'
-chef_version     '>= 12.21'
+chef_version     '>= 12.18'
 supports 'ubuntu', '>= 14.04'
 supports 'debian', '>= 9.0'
 supports 'centos', '>= 6.0'
